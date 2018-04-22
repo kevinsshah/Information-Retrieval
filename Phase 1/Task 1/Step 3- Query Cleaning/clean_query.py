ede72ecf665375cd57dfadf606db8c1dd1e2bbf5
twishavyas <vyas.tw@husky.neu.edu>
2018-04-21 20:04:47 -0400

    Clean Query

---
+++ Phase 1/Task 1/Step 3- Query Cleaning/clean_query.py
@@ -0,0 +1,53 @@
+import os.path as path
+import os
+import re
+
+def read_file_initialize_queries():
+    final_query = dict()
+    file = open("cacm.query.txt", "r")
+    queries = file.read()
+    queries = queries.replace('<DOC>', '').replace('\n', ' ').replace('\t', ' ').replace('<DOCNO>', '')
+    query_array = queries.split("</DOC>")
+    del query_array[-1]
+    for query in query_array:
+        temp = query.split('</DOCNO>')
+        key = temp[0].replace(' ', '')
+        value = temp[1].strip()
+        print(value)
+        value = cleanup(value, True, True)
+        print(value)
+        final_query[key] = value
+    write_queries_to_file(final_query)
+
+
+def write_queries_to_file(final_query):
+    fname = "cleanQueries.txt"
+    ofile = open(fname, 'w')
+    for key, value in final_query.items():
+       line = key + "||" + value + "\n"
+       ofile.write(line)
+
+
+def handle_punctuations(content):
+    result = re.sub(r"[^0-9A-Za-z,-\.:\\$]"," ",str(content))  # retain alpha-numeric text along with ',',':' and '.'
+    result = re.sub(r"(?!\d)[$,%,:.,-](?!\d)", " ", str(result), 0)  # retain '.', '-' or ',' between digits
+    result = result.split()
+
+    result_text = ' '.join(result)
+    return result_text
+
+
+# handle case folding and punctuations
+def cleanup(content, case_folding, punc_handling):
+    result = content
+    # perform case folding if wanted by the user
+    if case_folding:
+        result = result.lower()
+    # handle punctuations if wanted by the user
+    if punc_handling:
+        result = handle_punctuations(result)
+    return result
+
+
+
+read_file_initialize_queries()
